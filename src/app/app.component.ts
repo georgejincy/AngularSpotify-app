5607cd577d53f61105436f187ee5dfeb21709b1d
georgejincy <jincydon101@gmail.com>
2017-06-27 13:04:54 -0400

    Set components in app.module and services in app.component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
+import { SpotifyService } from './services/spotify.service';
+
+
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
+  providers: [SpotifyService]
 })
 export class AppComponent  { name = 'Angular'; }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { NavbarComponent }  from './components/navbar.component';
+import { SearchComponent }  from './components/search.component';
+import { AboutComponent }  from './components/about.component';
+import { AlbumComponent }  from './components/album.component';
+import { ArtistComponent }  from './components/artist.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, 
+  					NavbarComponent,
+  					SearchComponent,
+  					AboutComponent,
+  					AlbumComponent,
+  					ArtistComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
